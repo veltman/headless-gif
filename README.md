f026dc6b40fb28e39abdb43ebdf541f5f5c24dcc
Noah <noah@noahveltman.com>
2016-02-05 16:25:05 -0500

    Works

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # headless-gif
+
 Saving web worker'd gif via PhantomJS.
+
+Slight modification to the original page converts the gif result to a base64 global for PhantomJS ([example](https://bl.ocks.org/veltman/63c1ace02b74988fcddf)).
+
+PhantomJS grabs that and sends to stdout.  Decode that and then have a gif party.
+
+```
+$ phantomjs capture.js | base64 --decode > what-a-cool.gif
+```
+
+See: https://twitter.com/ivarvong/status/695682484554637312
---
+++ capture.js
@@ -0,0 +1,45 @@
+var fs = require("fs"),
+    page = require("webpage").create();
+
+// Dimensions don't really matter but need to be non-zero
+var width = 1,
+    height = 1,
+    url = "https://bl.ocks.org/veltman/raw/63c1ace02b74988fcddf/";
+
+page.viewportSize = { width: width, height: height};
+
+page.open(url, function (status) {
+
+  var waited = 0,
+      timeout = 1200000,
+      interval = 100;
+
+  // Wait until the base64 is ready
+  setInterval(function() {
+
+    waited += interval;
+
+    var base64 = page.evaluate(function(){
+
+      return phantomReady;
+
+    });
+
+    if (base64) {
+
+      // Write base64 to stdout for decoding
+      fs.write("/dev/stdout",base64);
+
+      phantom.exit();
+
+    } else if (waited > timeout) {
+
+      // Give up, something's wrong
+      console.warn("Maximum timeout of "+Math.round(timeout/1000)+"s exceeded.");
+      phantom.exit();
+
+    }
+
+  },interval);
+
+});
